4c78397600d54454628575bd65e19209d5655ff4
Benen Sullivan <Ranzadet@gmail.com>
2022-04-19 16:09:09 -0400

    updated Tape, added constructor and method

--- State.java
+++ State.java
@@ -1,8 +1,15 @@
 package DaringDuck;
 public class State{
-    public String name;
-    
-    public State(){
 
+    public String name;
+    public Transition[] transitions;
+    public boolean isTerminal;
+
+    public State(String name, Transition[] transitions, boolean isTerminal){
+        this.name = name;
+        this.transitions = transitions;
+        this.isTerminal = isTerminal;
     }
+
+    
 }
--- Tape.java
+++ Tape.java
@@ -1,4 +1,16 @@
 package DaringDuck;
+import java.util.ArrayList;
 public class Tape{
 
+    public static ArrayList<Character> tape = new ArrayList<>();
+    public long oneCount;
+
+    public Tape(char[] initial){
+        tape.ensureCapacity(100);
+    }
+
+    //This method should be called 
+    public void quickExpand(){
+        tape.ensureCapacity(tape.size()*tape.size());
+    }
 }
