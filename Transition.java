dc2db54d16fba9c2562525a868705818f9002646
Benen Sullivan <Ranzadet@gmail.com>
2022-04-19 23:10:37 -0400

    (possibly) finished Transition class

--- Transition.java
+++ Transition.java
@@ -1,4 +1,18 @@
 package DaringDuck;
 public class Transition{
     
+    public State from;
+    public State result;
+    public Character input;
+    public int direction; //Although an int, direction will be either -1 (left) or 1 (right), to indicate movement along the tape 
+    public Character write;
+
+    public Transition(State from, Character input, State result, Character write, int direction){
+        this.from = from;
+        this.input = input;
+        this.result = result;
+        this.write = write;
+        this.direction = direction;
+    }
+
 }
