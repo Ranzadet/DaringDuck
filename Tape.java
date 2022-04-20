fae563106e427d9adc1e9da8b22600bd88f09b95
Benen Sullivan <Ranzadet@gmail.com>
2022-04-19 22:53:59 -0400

    Added Expand() method, which will increase the size of the tape for the turing machine and return the index of the tapehead

--- Tape.java
+++ Tape.java
@@ -5,16 +5,38 @@ import java.util.Arrays;
 public class Tape{
 
     private static ArrayList<Character> tape;
-    public long oneCount;
+    public int oneCount;
+    public int pointer;
 
     public Tape(char[] initial){
         tape.ensureCapacity(100);
         tape = new ArrayList<>(Collections.nCopies(100, '0'));
     }
 
-    //This method should be called when the pointer attempts to move off the left side of the tape
-    public void Expand(){
-        tape.ensureCapacity(tape.size()*tape.size());
+    //This method should be called when the pointer attempts to move off either side of the tape
+    public int Expand(){
+        tape.ensureCapacity(tape.size()*3 + 1);
+        ArrayList<Character> left = new ArrayList<Character>(Collections.nCopies(tape.size(), '0'));
+        left.addAll(tape);
+        int index = left.size() - 1;
+        left.addAll(Collections.nCopies(tape.size(), '0'));
+        tape = left;
+        return index;
+    }
+
+    //to be called for increased effeciency, when the side of expansion is given
+    public int Expand(boolean norm){
+        int index = tape.size();
+        tape.ensureCapacity(tape.size()*2 + 1);
+        if(norm){
+            tape.addAll(Collections.nCopies(tape.size(), '0'));
+        }
+        else{
+            ArrayList<Character> left = new ArrayList<Character>(Collections.nCopies(tape.size(), '0'));
+            left.addAll(tape);
+            tape = left;
+        }
+        return index;
     }
 
     public Character readTape(int index){
