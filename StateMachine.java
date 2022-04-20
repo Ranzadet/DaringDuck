7bd2402d1deec2532d202b43031e854b1b5a8b66
Benen Sullivan <Ranzadet@gmail.com>
2022-04-19 21:23:03 -0400

    Added multiple methods to Tape class, implemented tape as an arraylist of Characters, with '0' representing the null character

--- StateMachine.java
+++ StateMachine.java
@@ -1,4 +1,12 @@
 package DaringDuck;
+import java.util.ArrayList;
 public class StateMachine{
+
+    public ArrayList<State> states = new ArrayList<>();
+    public int currentState = 0;
+
+        public StateMachine(){
+            
+        }
 
 }
--- Tape.java
+++ Tape.java
@@ -1,16 +1,29 @@
 package DaringDuck;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
 public class Tape{
 
-    public static ArrayList<Character> tape = new ArrayList<>();
+    private static ArrayList<Character> tape;
     public long oneCount;
 
     public Tape(char[] initial){
         tape.ensureCapacity(100);
+        tape = new ArrayList<>(Collections.nCopies(100, '0'));
     }
 
-    //This method should be called 
-    public void quickExpand(){
+    //This method should be called when the pointer attempts to move off the left side of the tape
+    public void Expand(){
         tape.ensureCapacity(tape.size()*tape.size());
     }
+
+    public Character readTape(int index){
+        return tape.get(index);
+    }
+
+    public void writeTape(Character c, int index){
+        tape.set(index, c);
+    }
+
+
 }
